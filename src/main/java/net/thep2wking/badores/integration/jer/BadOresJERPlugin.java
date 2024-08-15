0db1c03cbfff8255b0660e533d5984f1c8af0484
TheP2WKing <tdziomba@hotmail.com>
2024-08-15 20:40:24 +0200

    formatting

--- src/main/java/net/thep2wking/badores/integration/jer/BadOresJERPlugin.java
+++ src/main/java/net/thep2wking/badores/integration/jer/BadOresJERPlugin.java
@@ -22,22 +22,20 @@ public class BadOresJERPlugin extends ModJERPluginBase {
 	@Override
 	public void register() {
 		if (BadOresConfig.INTEGRATION.JER_INTEGRATION) {
-			addMob(new EntityFleesonsite(getWorld()), LightLevel.any, EntityFleesonsite.EXPERIENCE_VALUE,
-					EntityFleesonsite.LOOT_TABLE);
-			addMob(new EntityNosleeptonite(getWorld()), LightLevel.any, EntityNosleeptonite.EXPERIENCE_VALUE,
-					EntityNosleeptonite.LOOT_TABLE);
+			addMob(new EntityFleesonsite(getWorld()), LightLevel.any, EntityFleesonsite.EXPERIENCE_VALUE, EntityFleesonsite.LOOT_TABLE);
+			addMob(new EntityNosleeptonite(getWorld()), LightLevel.any, EntityNosleeptonite.EXPERIENCE_VALUE, EntityNosleeptonite.LOOT_TABLE);
 
 			addOreGen(ModBlocks.POLITE_ORE, 6, 6, 1, 64);
-			addOreGen(ModBlocks.WANNAFITE_ORE, 6, 6, 1, 64,
-					new LootDrop[] { new LootDrop(new ItemStack(Items.IRON_SWORD, 1, 0)) });
+			addOreGen(ModBlocks.WANNAFITE_ORE, 6, 6, 1, 64, new LootDrop[] { 
+				new LootDrop(new ItemStack(Items.IRON_SWORD, 1, 0)) });
 			addOreGen(ModBlocks.BREAKIUM_ORE, 6, 6, 1, 64);
-			addOreGen(ModBlocks.STONIUM_ORE, 15, 60, 32, 256,
-					new LootDrop[] { new LootDrop(new ItemStack(Blocks.COBBLESTONE, 1, 0)) });
+			addOreGen(ModBlocks.STONIUM_ORE, 15, 60, 32, 256, new LootDrop[] { 
+				new LootDrop(new ItemStack(Blocks.COBBLESTONE, 1, 0)) });
 			addOreGen(ModBlocks.CRAPPIUM_ORE, 8, 8, 40, 120);
 			addOreGen(ModBlocks.ENDERITE_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.WEBSITE_ORE, 6, 6, 1, 64);
-			addOreGen(ModBlocks.LITE_ORE, 4, 4, 1, 48,
-					new LootDrop[] { new LootDrop(new ItemStack(Items.GLOWSTONE_DUST, 1, 0)) });
+			addOreGen(ModBlocks.LITE_ORE, 4, 4, 1, 48, new LootDrop[] { 
+				new LootDrop(new ItemStack(Items.GLOWSTONE_DUST, 1, 0)) });
 			addOreGen(ModBlocks.MISLEADIUM_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.GHOSTIUM_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.AMADEUM_ORE, 5, 6, 4, 64);
@@ -48,11 +46,11 @@ public class BadOresJERPlugin extends ModJERPluginBase {
 			addOreGen(ModBlocks.KILLIUM_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.MOVIUM_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.BALANCIUM_ORE, 4, 3, 1, 48, new LootDrop[] {
-					new LootDrop(new ItemStack(Items.COAL, 5, 0)), new LootDrop(new ItemStack(Items.IRON_INGOT, 5, 0)),
-					new LootDrop(new ItemStack(Items.GOLD_INGOT, 5, 0)),
-					new LootDrop(new ItemStack(Items.DIAMOND, 5, 0)), new LootDrop(new ItemStack(Items.EMERALD, 5, 0)),
-					new LootDrop(new ItemStack(Items.QUARTZ, 5, 0)),
-					new LootDrop(new ItemStack(Items.GOLDEN_APPLE, 5, 0)) });
+				new LootDrop(new ItemStack(Items.COAL, 5, 0)), new LootDrop(new ItemStack(Items.IRON_INGOT, 5, 0)),
+				new LootDrop(new ItemStack(Items.GOLD_INGOT, 5, 0)),
+				new LootDrop(new ItemStack(Items.DIAMOND, 5, 0)), new LootDrop(new ItemStack(Items.EMERALD, 5, 0)),
+				new LootDrop(new ItemStack(Items.QUARTZ, 5, 0)),
+				new LootDrop(new ItemStack(Items.GOLDEN_APPLE, 5, 0)) });
 			addOreGen(ModBlocks.EXPLODEITMITE_ORE, 2, 3, 1, 48);
 			addSpikeOreGen(ModBlocks.MARMITE_ORE, 55, 20, 0.0001f);
 			addOreGen(ModBlocks.SHIFTIUM_ORE, 6, 6, 1, 64);
@@ -62,28 +60,28 @@ public class BadOresJERPlugin extends ModJERPluginBase {
 			addOreGen(ModBlocks.STREETSCUM_ORE, 5, 6, 1, 48);
 			addOreGen(ModBlocks.FLEESONSITE_ORE, 5, 5, 1, 64);
 			addOreGen(ModBlocks.NOPIUM_ORE, 6, 6, 1, 64);
-			addOreGen(ModBlocks.ZOMBIEUNITE_ORE, 4, 6, 1, 64,
-					new LootDrop[] { new LootDrop(new ItemStack(Items.SKULL, 1, 2)) });
-			addSpikeOreGen(ModBlocks.PAINTITWHITE_ORE, 60, 40, 0.0002f,
-					new LootDrop[] { new LootDrop(new ItemStack(Items.DYE, 1, 15)) });
+			addOreGen(ModBlocks.ZOMBIEUNITE_ORE, 4, 6, 1, 64, new LootDrop[] { 
+				new LootDrop(new ItemStack(Items.SKULL, 1, 2)) });
+			addSpikeOreGen(ModBlocks.PAINTITWHITE_ORE, 60, 40, 0.0002f, new LootDrop[] { 
+				new LootDrop(new ItemStack(Items.DYE, 1, 15)) });
 			addOreGen(ModBlocks.IWONTFITE_ORE, 6, 6, 1, 64);
 			addOreGen(ModBlocks.TAUNTUM_ORE, 4, 4, 1, 48);
-			addOreGen(ModBlocks.PANDAEMONIUM_ORE, 4, 6, 1, 10,
-					new LootDrop[] { new LootDrop(new ItemStack(Blocks.NETHERRACK, 2, 0)),
-							new LootDrop(new ItemStack(Blocks.SOUL_SAND, 2, 0)),
-							new LootDrop(new ItemStack(Blocks.OBSIDIAN, 2, 0)),
-							new LootDrop(new ItemStack(Items.FIRE_CHARGE, 2, 0)),
-							new LootDrop(new ItemStack(Items.BLAZE_ROD, 2, 0)),
-							new LootDrop(new ItemStack(Items.MAGMA_CREAM, 2, 0)),
-							new LootDrop(new ItemStack(Items.NETHER_WART, 2, 0)) });
+			addOreGen(ModBlocks.PANDAEMONIUM_ORE, 4, 6, 1, 10, new LootDrop[] { 
+				new LootDrop(new ItemStack(Blocks.NETHERRACK, 2, 0)),
+				new LootDrop(new ItemStack(Blocks.SOUL_SAND, 2, 0)),
+				new LootDrop(new ItemStack(Blocks.OBSIDIAN, 2, 0)),
+				new LootDrop(new ItemStack(Items.FIRE_CHARGE, 2, 0)),
+				new LootDrop(new ItemStack(Items.BLAZE_ROD, 2, 0)),
+				new LootDrop(new ItemStack(Items.MAGMA_CREAM, 2, 0)),
+				new LootDrop(new ItemStack(Items.NETHER_WART, 2, 0)) });
 			addOreGen(ModBlocks.NOSLEEPTONITE_ORE, 2, 3, 1, 40);
 			addSpikeOreGen(ModBlocks.APPETITE_ORE, 55, 20, 0.0002f);
 			addOreGen(ModBlocks.CRASHIUM_ORE, 6, 5, 1, 64);
 			addOreGen(ModBlocks.SMITE_ORE, 6, 6, 1, 64);
-			addOreGen(ModBlocks.DIAMOND_ORE, 6, 6, 1, 64,
-					new LootDrop[] { new LootDrop(new ItemStack(ModItems.DIAMOND, 1, 0)) });
-			addOreGen(ModBlocks.KAKKARITE_ORE, 6, 5, 1, 64, new LootDrop[] { new LootDrop(
-					new ItemStack(ModItems.KAKKARITE_GEMSTONE, BadOresConfig.EVENTS.KAKKARITE_DROP_COUNT, 0)) });
+			addOreGen(ModBlocks.DIAMOND_ORE, 6, 6, 1, 64, new LootDrop[] {
+				new LootDrop(new ItemStack(ModItems.DIAMOND, 1, 0)) });
+			addOreGen(ModBlocks.KAKKARITE_ORE, 6, 5, 1, 64, new LootDrop[] { 
+				new LootDrop(new ItemStack(ModItems.KAKKARITE_GEMSTONE, BadOresConfig.EVENTS.KAKKARITE_DROP_COUNT, 0)) });
 		}
 	}
 }
