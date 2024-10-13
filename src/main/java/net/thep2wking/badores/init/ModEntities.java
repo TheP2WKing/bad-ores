3d0e3b52aacdac4902b749db4454555dc320a49d
TheP2WKing <tdziomba@hotmail.com>
2024-10-14 00:02:55 +0200

    entity registry id++

--- src/main/java/net/thep2wking/badores/init/ModEntities.java
+++ src/main/java/net/thep2wking/badores/init/ModEntities.java
@@ -10,7 +10,8 @@ public class ModEntities {
 	public static void registerEntities() {
 		ModLogger.registeredEntitiesLogger(BadOres.MODID);
 
-		ModRegistryHelper.registerEntityWithSpawnEgg(BadOres.MODID, "fleesonsite", BadOres.INSTANCE, 0, EntityFleesonsite.class, 64, 3, true, 0x797979, 0x25c8d1);
-		ModRegistryHelper.registerEntityWithSpawnEgg(BadOres.MODID, "nosleeptonite", BadOres.INSTANCE, 1, EntityNosleeptonite.class, 64, 3, true, 0x797979, 0xa33323);
+		int id = 0;
+		ModRegistryHelper.registerEntityWithSpawnEgg(BadOres.MODID, "fleesonsite", BadOres.INSTANCE, id++, EntityFleesonsite.class, 64, 3, true, 0x797979, 0x25c8d1);
+		ModRegistryHelper.registerEntityWithSpawnEgg(BadOres.MODID, "nosleeptonite", BadOres.INSTANCE, id++, EntityNosleeptonite.class, 64, 3, true, 0x797979, 0xa33323);
 	}
 }
